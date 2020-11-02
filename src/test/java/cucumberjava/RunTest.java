52bc4f195d2eb9675669b919727633d89b990829
MacBook Air <macbookair@MacBook-Air.local>
2020-11-03 01:28:21 +0530

    Added report json

--- src/test/java/cucumberjava/RunTest.java
+++ src/test/java/cucumberjava/RunTest.java
@@ -4,6 +4,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty","json:./results/cucumber.json"})
 public class RunTest{
 }
